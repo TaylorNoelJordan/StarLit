679bc6a88911f63aa4e8f05223685b6e94808239
Taylor Jordan <taylornoel418@icloud.com>
2019-07-25 21:38:41 -0600

    Add actions and connect HoroscopeForm

--- src/components/HoroscopeForm/HoroscopeForm.js
+++ src/components/HoroscopeForm/HoroscopeForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import{ postSign } from '../../../utilz/apiCalls';
+import { connect } from 'react-redux';
+import { setUser, hasErrored } from '../../actions'
 import './HoroscopeForm.css'
 
 class HoroscopeForm extends React.Component {
@@ -14,8 +17,15 @@ class HoroscopeForm extends React.Component {
         this.setState({[e.target.name]: e.target.value})
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
+        const { sign } = this.state;
+        try {
+            let user = await postSign(sign);
+            this.props.setUser(user)
+        } catch ({ message }) {
+            this.props.hasErrored(message)
+        }
     }
 
     render() {
@@ -28,17 +38,17 @@ class HoroscopeForm extends React.Component {
                     <select name='sign' value={this.state.sign} onChange={this.handleChange} className='horoscope-form-input'>
                         <option>Choose One...</option>
                         <option value='aries'>Aries (Mar 21-Apr 19)</option>
-                        <option onChange={this.handleChange} value='taurus'>Taurus (Apr 20-May 20)</option>
-                        <option onChange={this.handleChange} value='gemini'>Gemini (May 21-June 20)</option>
-                        <option onChange={this.handleChange} value='cancer'>Cancer (June 21-July 22)</option>
-                        <option onChange={this.handleChange} value='leo'>Leo (July 23-Aug 22)</option>
-                        <option onChange={this.handleChange} value='virgo'>Virgo (Aug 23-Sept 22)</option>
-                        <option onChange={this.handleChange} value='libra'>Libra (Sept 23-Oct 22)</option>
-                        <option onChange={this.handleChange} value='scorpio'>Scorpio (Oct 23-Nov 21)</option>
-                        <option onChange={this.handleChange} value='sagittarius'>Sagittarius (Nov 22-Dec 21)</option>
-                        <option onChange={this.handleChange} value='capricorn'>Capricorn (Dec 22-Jan 19)</option>
-                        <option onChange={this.handleChange} value='aquarius'>Aquarius (Jan 20-Feb 18)</option>
-                        <option onChange={this.handleChange} value='pisces'>Pisces (Feb 19-Mar 20)</option>
+                        <option value='taurus'>Taurus (Apr 20-May 20)</option>
+                        <option value='gemini'>Gemini (May 21-June 20)</option>
+                        <option value='cancer'>Cancer (June 21-July 22)</option>
+                        <option value='leo'>Leo (July 23-Aug 22)</option>
+                        <option value='virgo'>Virgo (Aug 23-Sept 22)</option>
+                        <option value='libra'>Libra (Sept 23-Oct 22)</option>
+                        <option value='scorpio'>Scorpio (Oct 23-Nov 21)</option>
+                        <option value='sagittarius'>Sagittarius (Nov 22-Dec 21)</option>
+                        <option value='capricorn'>Capricorn (Dec 22-Jan 19)</option>
+                        <option value='aquarius'>Aquarius (Jan 20-Feb 18)</option>
+                        <option value='pisces'>Pisces (Feb 19-Mar 20)</option>
                     </select>
                     <button className='horoscope-form-submit' onClick={e => this.handleSubmit(e)}>Skeddit!</button>
                 </form>
@@ -47,4 +57,12 @@ class HoroscopeForm extends React.Component {
     }
 }
 
-export default HoroscopeForm;
+const mapStateToProps = state => ({
+    user: state.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    setUser: user => dispatch(setUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HoroscopeForm);
---
+++ utilz/apiCalls.js
@@ -0,0 +1,20 @@
+export const postSign = async (sign) => {
+  try {
+    const url = `https://aztro.sameerkumar.website/?sign=${sign}&day=today`
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({sign: sign})
+    }
+
+    const getData = await fetch(url, options)
+    if(!getData.ok) {
+      throw new Error('Something went wrong... but we\'ll give you a do-over.')
+    }
+    const response = getData.json();
+    return response
+  } catch (error) {
+    throw Error(error.message)
+  }
+
+}
