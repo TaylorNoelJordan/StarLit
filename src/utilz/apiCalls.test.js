553e444e5f937abd228542fcb29adbff085d3552
Taylor Jordan <taylornoel418@icloud.com>
2019-07-26 22:07:21 -0600

    Add testing for postSign method

--- src/utilz/apiCalls.test.js
+++ src/utilz/apiCalls.test.js
@@ -1,22 +1,42 @@
 import { postSign } from './apiCalls'
-import { shallow } from enzyme;
+// import { shallow } from 'enzyme';
 
-describe('postUser', () => {
+describe('postSign', () => {
     let mockResponse;
-    let sign;
 
     beforeEach(() => {
-        sign = 'libra';
         mockResponse = {description: 'You\'ve been working hard. Treat yourself to a caramal macchiatto today.', color: 'gold', time: '7am'};
         window.fetch = jest.fn().mockImplementation(() => {
-            Promise.resolve({
+            return Promise.resolve({
                 ok: true,
-                json: () => Promise.resolve({ mockResponse})
+                json: () => Promise.resolve(mockResponse)
             })
         })
-    })
+    });
 
     it('should be called with appropriate params', () => {
+        const expected = [`https://aztro.sameerkumar.website/?sign=undefined&day=today`,
+            { method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: '{}'
+         }
+    ]
+        postSign();
+        expect(window.fetch).toHaveBeenCalledWith(...expected)
+    });
+
+    it('HAPPY: should return a response if status is ok', async () => {
+        const result = await postSign();
+        expect(result).toEqual(mockResponse)
+    });
+
+    it('SAD: should return an error if status is not ok', () => {
+        window.fetch = jest.fn().mockImplementationOnce(() => {
+            return Promise.resolve({
+                ok: false
+            })
+        })
         
+        expect(postSign()).rejects.toEqual('Something went wrong... but we\'ll give you a do-over.')
     })
 })
