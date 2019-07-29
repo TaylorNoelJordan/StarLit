bdf044db2905f1b84878582e7021e3cfdaa1324e
Taylor Jordan <taylornoel418@icloud.com>
2019-07-28 22:38:36 -0600

    Add testing suite for details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './Details.css';
 
 export const Details = (props) => {
-    console.log(props)
     const { time, color, mood, compatibility } = props
     return (
         <section className="yes-details">
@@ -11,6 +11,9 @@ export const Details = (props) => {
             <h3 className='yes-details-topic'>Color to wear: {color}</h3>
             <h3 className='yes-details-topic'>Time you should go out: {time}</h3>
             <h3 className='yes-details-topic'>Sign to link up with (friends or more than friends 😈): {compatibility}</h3>
+            <Link to='/'>
+                <button>Start Over</button>
+            </Link>
         </section>
     )
 }
---
+++ src/components/Details/Details.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
+import { Details, mapStateToProps } from './Details';
+
+describe('Details', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Details/>)
+    });
+
+    it('should match the snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should return the user to the home page on start over btn', () => {
+        expect(wrapper.find(Link).props().to).toBe('/')
+    });
+
+    describe('mapStateToProps', () => {
+        it('should return corresponding portions of state', () => {
+            const mockState = {
+                user: {
+                    lucky_time: '11am',
+                    color: 'orange',
+                    mood: 'confident',
+                    compatibility: 'Pisces'
+                }
+            }
+
+            const expected = {
+                time: mockState.user.lucky_time,
+                color: mockState.user.color,
+                mood: mockState.user.mood,
+                compatibility: mockState.user.compatibility
+            }
+
+            const mappedProps = mapStateToProps(mockState);
+            expect(mappedProps).toEqual(expected)
+
+        })
+    });
+})
